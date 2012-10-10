3673b05a7fe650c1b4eec98a45788e5c746484db
stargazer <already.late@gmail.com>
2012-10-10 13:44:54 +0200

    Removed namespace declaration

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     author='C. Paschalides',
     url='http://github.com/stargazer/s3tools-py',
     packages=find_packages(),
-    namespace_packages=('s3tools',),
     install_requires=(
         'boto',
     ),
