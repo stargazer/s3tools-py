866ea01e74a89cdd5f8359f12d9bd03f972ec58f
stargazer <already.late@gmail.com>
2012-10-12 11:43:12 +0200

    docstrings

--- s3tools/tools.py
+++ s3tools/tools.py
@@ -7,13 +7,29 @@ class S3Tools:
     Class that implements some lowlevel Amazon S3 functionality.
     """
     def __init__(self, AWS_access, AWS_secret):
+        """
+        Initializes the ``S3Tools`` object with the AWS credentials.
+
+        :param AWS_access:  AWS access key
+        :param AWS_secret:  AWS secret key
+
+        :type AWS_access:   string
+        :type AWS_secret:   string
+        """
         self.AWS_access = AWS_access
         self.AWS_secret = AWS_secret
 
     def _connect_and_get_bucket(self, bucket_name):
         """
-        @param bucket_name: Bucket name
-        @return: Bucket instance
+        Connects to Amazon S3, and returns a Bucket instance.
+
+        :param bucket_name: Name of bucket
+
+        :type bucket_name:  string
+
+        :return:            Bucket 
+        :rtype:             Bucket
+        :raises:            BotoClienterror
         """
         try:
             conn = S3Connection(
@@ -29,13 +45,19 @@ class S3Tools:
         """
         Creates a file on Amazon S3. 
 
-        @param bucket_name: Bucket to use (string)
-        @param key: Key to use (string)
-        @param data: File contents
-        @param headers: File headers (dict)
-        @param acl: File permissions (string). Any string of:('private', 'public-read',
-            'public-read-write', 'authenticated-read', 'bucket-owner-read', 
-            'bucket-owner-full-control', 'log-delivery-write')
+        :param bucket_name: Name of bucket to use
+        :param key:         Key to use
+        :param data:        File contents
+        :param headers:     File headers
+        :param acl:         File permissions.
+
+        :type bucket_name:  string
+        :type key:          string
+        :type data:         anything
+        :type headers:      dict
+        :type acl:          string. Any string of: ('private', 'public-read', 'public-read-write', 'authenticated-read', 'bucket-owner-read', 'bucket-owner-full-control', 'log-delivery-write')
+
+        :raises:            BotoClientError
         """        
         # Returns bucket connection
         bucket = self._connect_and_get_bucket(bucket_name)
@@ -57,10 +79,16 @@ class S3Tools:
 
     def delete(self, bucket_name, key):
         """
-        Deletes a file from Amazon S3
+        Deletes a file from Amazon S3.
 
-        @param bucket_name: Name of the bucket (string)
-        @param key: Key to delete (string)
+        :param bucket_name: Name of the bucket
+        :param key:         Key to delete
+
+        :type bucket_name:  string
+        :type key:          string
+
+        :raises:            BotoClientError
+        
         """
         bucket = self._connect_and_get_bucket(bucket_name)
 
